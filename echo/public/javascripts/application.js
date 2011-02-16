c1a1e342e5a495a59774f5763935f52856868dae
brandon <brandon@bethel-churchs-imac.local>
2011-02-16 08:56:57 -0800

    Caching stylesheets and javascript

--- echo/public/javascripts/application.js
+++ echo/public/javascripts/application.js
@@ -1,6 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(function clippyCopiedCallback(a){var d=$("#clippy_tooltip_"+a);if(d.length!=0){d.attr("title","copied!").trigger("tipsy.reload");setTimeout(function(){d.attr("title","copy to clipboard")},500)}};GitHub.Autocomplete=function(){}
-
+$(function(){
+	$('#clippy-tooltip').tipsy({gravity: 's'});
 });
