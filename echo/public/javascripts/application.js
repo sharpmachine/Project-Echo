827d10d5f381283e40f8d356f4f245388b3405b5
sharpmachine <jessekade@gmail.com>
2011-03-01 21:48:52 -0800

    coded email a friend button

--- echo/public/javascripts/application.js
+++ echo/public/javascripts/application.js
@@ -2,5 +2,17 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(function(){
-	$('#clippy-tooltip').tipsy({gravity: 's'});
+	$('.clippy-tooltip').tipsy({gravity: 's'});
 });
+
+jQuery.fn.fadeToggle = function(speed, easing, callback) {
+  return this.animate({opacity: 'toggle'}, speed, easing, callback);  
+};
+ 
+$(document).ready(function() {
+	$('#tellfriend').hide();
+  	$(' a.email, #tellfriend a.close').click(function() {
+    $("#tellfriend").fadeToggle('slow');
+  });
+  
+}); 
