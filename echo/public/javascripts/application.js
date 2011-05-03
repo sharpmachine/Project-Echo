f60f7dcee55ddac9ba6e814112cdd38fcd20f179
sharpmachine <jessekade@gmail.com>
2011-05-03 00:01:42 -0700

    worked on progress bar for front page

--- echo/public/javascripts/application.js
+++ echo/public/javascripts/application.js
@@ -16,3 +16,16 @@ $(document).ready(function() {
   });
   
 }); 
+
+$(function(){
+	
+	var x = $('.project-progress').attr('id');
+	var y = (Math.floor(x/2133*100));
+	
+	$('.project-progress').each(function(){
+	
+		$(".project-progress").width(y+"%");
+		$(".progress-percentage").prepend(y+'%');
+	return false;
+	});
+});
